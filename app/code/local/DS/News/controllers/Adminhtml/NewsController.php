c22f3005a3ab7717cd0ae85b116ea4f1300d1697
sergey <sergeyman78@gmail.com>
2018-05-14 00:25:20 +0300

    Adding Admin Controller

---
+++ app/code/local/DS/News/controllers/Adminhtml/NewsController.php
@@ -0,0 +1,6 @@
+<?php
+class DS_News_Adminhtml_NewsController extends Mage_Adminhtml_Controller_Action {
+    public function indexAction() {
+        echo '<h1>News Module: Admin dection</h1>';
+    }
+}
